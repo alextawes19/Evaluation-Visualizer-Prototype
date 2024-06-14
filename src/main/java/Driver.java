7264f89ca6a848aaa7ae8793bbdd9507deb6241f
Alex Tawes <atawes@students.kennesaw.edu>
2024-06-14 15:43:35 -0400

    Basic statistic reporting

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,2 +1,78 @@
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * Main driver for the application.
+ */
 public class Driver {
-}
+    public static void main(String[] args) {
+        //START TIMER
+        long startTime = System.nanoTime();
+
+        ArrayList<WorkOrder> workOrders = new ArrayList<>();
+
+        double numQ6 = 0;
+        String q6 = "Overall Performance Was:";
+
+        //csv file handling
+        try {
+            File csvFile = new File("src/main/resources/TwoDays.csv");
+            FileReader fileReader = new FileReader(csvFile);
+            CSVReader csvReader = new CSVReader(fileReader);
+            String[] nextRecord;
+            //skip first line of csv
+            csvReader.readNext();
+
+            while ((nextRecord = csvReader.readNext()) != null) {
+                WorkOrder workOrder = new WorkOrder (
+                        nextRecord[0], nextRecord[1], nextRecord[2], nextRecord[3],
+                        nextRecord[4],nextRecord[5], nextRecord[6], nextRecord[7],
+                        nextRecord[8],nextRecord[9], nextRecord[10], nextRecord[11],
+                        nextRecord[12],nextRecord[13], nextRecord[14]
+                );
+                workOrders.add(workOrder);
+
+                if (workOrder.getEvalQuestTxt().equals(q6)) {
+                    numQ6++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Unexpected error has occurred.");
+        } //try
+
+        int numExcellent = 0;
+        int numGood = 0;
+        int numPoor = 0;
+
+        for (WorkOrder wo : workOrders) {
+            if (wo.getEvalQuestValTxt().equals("Excellent")) {
+                numExcellent++;
+            } else if (wo.getEvalQuestValTxt().equals("Good")) {
+                numGood++;
+            } else if (wo.getEvalQuestValTxt().equals("Poor")) {
+                numPoor++;
+            }
+        }
+        double size = workOrders.size();
+        double percentExcellent = (numExcellent/numQ6) * 100;
+        double percentGood = (numGood/numQ6) * 100;
+        double percentPoor = (numPoor/numQ6) * 100;
+
+        System.out.println(workOrders.get(0).toString());
+        System.out.println("# of WOs: " + workOrders.size());
+        System.out.println("-----STATISTICS-----");
+        System.out.println("Excellent: " + numExcellent + "(" + percentExcellent + ")");
+        System.out.println("Good: " + numGood + "(" + percentGood + ")");
+        System.out.println("Poor: " + numPoor + "(" + percentPoor + ")");
+
+
+        //END TIMER
+        long endTime = System.nanoTime();
+        long totalTime = (endTime - startTime)/1000000;
+        long totalTimeS = totalTime/1000;
+        System.out.println("Run time: " + totalTime + "ms, " + totalTimeS + "s");
+    } //main
+} //Driver
--- src/main/java/WorkOrder.java
+++ src/main/java/WorkOrder.java
@@ -1,2 +1,147 @@
+/**
+ * Represents a work order within the evaluation form and its response to a specific form question.
+ */
 public class WorkOrder {
-}
+    private final String workOrderID;
+    private final String evalQuestTxt;
+    private final String evalQuestValTxt;
+    private final String locationName;
+    private final String city;
+    private final String state;
+    private final String signDate;
+    private final String jobID;
+    private final String jobCategoryID;
+    private final String programType;
+    private final String customerName;
+    private final String primaryTechnicianID;
+    private final String districtName;
+    private final String woStatusID;
+    private final String gp;
+
+    /**
+     *  Constructs WorkOrder object.
+     *
+     * @param workOrderID work order ID
+     * @param evalQuestTxt eval question text
+     * @param evalQuestValTxt eval answer text
+     * @param locationName store name
+     * @param city city
+     * @param state state
+     * @param signDate date and time of signature
+     * @param jobID job ID
+     * @param jobCategoryID job category ID
+     * @param programType program type
+     * @param customerName customer name
+     * @param primaryTechnicianID technician username
+     * @param districtName district
+     * @param woStatusID WO status ID
+     * @param gp gross profit
+     */
+    public WorkOrder(String workOrderID, String evalQuestTxt, String evalQuestValTxt, String locationName,
+                     String city, String state, String signDate, String jobID, String jobCategoryID,
+                     String programType, String customerName, String primaryTechnicianID,
+                     String districtName, String woStatusID, String gp) {
+        this.workOrderID = workOrderID;
+        this.evalQuestTxt = evalQuestTxt;
+        this.evalQuestValTxt = evalQuestValTxt;
+        this.locationName = locationName;
+        this.city = city;
+        this.state = state;
+        this.signDate = signDate;
+        this.jobID = jobID;
+        this.jobCategoryID = jobCategoryID;
+        this.programType = programType;
+        this.customerName = customerName;
+        this.primaryTechnicianID = primaryTechnicianID;
+        this.districtName = districtName;
+        this.woStatusID = woStatusID;
+        this.gp = gp;
+    } //WorkOrder
+
+
+
+    // GETTERS
+    public String getWorkOrderID() {
+        return workOrderID;
+    } //getWorkOrderID
+
+    public String getEvalQuestTxt() {
+        return evalQuestTxt;
+    } //getEvalQuestTxt
+
+    public String getEvalQuestValTxt() {
+        return evalQuestValTxt;
+    } //getEvalQuestValTxt
+
+    public String getLocationName() {
+        return locationName;
+    } //getLocationName
+
+    public String getCity() {
+        return city;
+    } //getCity
+
+    public String getState() {
+        return state;
+    } //getState
+
+    public String getSignDate() {
+        return signDate;
+    } //getSignDate
+
+    public String getJobID() {
+        return jobID;
+    } //getJobID
+
+    public String getJobCategoryID() {
+        return jobCategoryID;
+    } //getJobCategoryID
+
+    public String getProgramType() {
+        return programType;
+    } //getProgramType
+
+    public String getCustomerName() {
+        return customerName;
+    } //getCustomerName
+
+    public String getPrimaryTechnicianID() {
+        return primaryTechnicianID;
+    } //getPrimaryTechnicianID
+
+    public String getDistrictName() {
+        return districtName;
+    } //getDistrictName
+
+    public String getWoStatusID() {
+        return woStatusID;
+    } //getWoStatusID
+
+    public String getGp() {
+        return gp;
+    } //getGp
+
+    //ADDITIONAL CLASS METHODS
+
+    @Override
+    public String toString() {
+        return "WorkOrder{" +
+                "workOrderID='" + workOrderID + '\'' +
+                ", evalQuestTxt='" + evalQuestTxt + '\'' +
+                ", evalQuestValTxt='" + evalQuestValTxt + '\'' +
+                ", locationName='" + locationName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", signDate='" + signDate + '\'' +
+                ", jobID='" + jobID + '\'' +
+                ", jobCategoryID='" + jobCategoryID + '\'' +
+                ", programType='" + programType + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", primaryTechnicianID='" + primaryTechnicianID + '\'' +
+                ", districtName='" + districtName + '\'' +
+                ", woStatusID='" + woStatusID + '\'' +
+                ", gp='" + gp + '\'' +
+                '}';
+    } //toString
+
+} //WorkOrder
