1a0b3afb84e299e5c0d5537c3a12900f9e2d00ea
Alex Tawes <atawes@students.kennesaw.edu>
2024-07-09 13:35:07 -0400

    File upload system implemented. Checks to make sure correct file and format is uploaded.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import com.opencsv.CSVReader;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main extends JFrame {
+    //Swing components
     private JButton buttonUpload;
     private JPanel panelMain;
     private JComboBox comboFilter;
@@ -13,6 +19,9 @@ public class Main extends JFrame {
     private JButton buttonFilter;
     private JTextArea textArea1;
     private JButton buttonExport;
+
+    //Utility
+    ArrayList<CombinedWorkOrder> combinedWorkOrders;
 
     public Main() {
         //topPanel
@@ -45,7 +54,7 @@ public class Main extends JFrame {
         Main m = new Main();
         m.setTitle("Eval Form Analysis");
         m.setContentPane(m.panelMain);
-        m.setSize(600, 600);
+        m.setSize(900, 900);
         m.setVisible(true);
         m.setResizable(false);
 
@@ -56,8 +65,53 @@ public class Main extends JFrame {
     public void uploadFile() {
         JFileChooser fileChooser = new JFileChooser();
         int returnValue = fileChooser.showOpenDialog(this);
-        if (returnValue == fileChooser.APPROVE_OPTION) {
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
+            if (!selectedFile.getName().endsWith(".csv")) {
+                JOptionPane.showMessageDialog(this, "Please upload a CSV file.");
+            } else {
+                //process csv
+                processCSV(selectedFile);
+            }
         }
     } //uploadFile
+
+    public void processCSV(File selectedFile) {
+        ArrayList<WorkOrder> workOrders = new ArrayList<>();
+        HashSet<String> rawWOs = new HashSet<>();
+
+        try {
+            FileReader fileReader = new FileReader(selectedFile);
+            CSVReader csvReader = new CSVReader(fileReader);
+            String[] nextRecord;
+
+            //ensure correct csv format
+            nextRecord = csvReader.readNext();
+
+            //take care of byte order mark (BOM)
+            if (nextRecord[0].startsWith("\uFEFF")) {
+                nextRecord[0] = nextRecord[0].substring(1);
+            }
+
+            if (!nextRecord[0].equals("WorkOrderID") || !nextRecord[1].equals("EvalQuestTxt") || !nextRecord[2].equals("EvalQuestValTxt")
+                    || !nextRecord[3].equals("LocationName") || !nextRecord[4].equals("City") || !nextRecord[5].equals("State")
+                    || !nextRecord[6].equals("SignDate") || !nextRecord[7].equals("JobID") || !nextRecord[8].equals("JobCategoryID")
+                    || !nextRecord[9].equals("ProgramType") || !nextRecord[10].equals("CustomerName") || !nextRecord[11].equals("PrimaryTechnicianID")
+                    || !nextRecord[12].equals("DistrictName") || !nextRecord[13].equals("WOStatusID") || !nextRecord[14].equals("Revenue")
+                    || !nextRecord[15].equals("Expense") || !nextRecord[16].equals("GP")) {
+
+                throw new Exception("CSV Format Error.\n\nExpected file layout:\n" +
+                        "Column 1: WorkOrderID\nColumn 2: EvalQuestTxt\nColumn 3: EvalQuestValTxt\nColumn 4: LocationName\n" +
+                        "Column 5: City\nColumn 6: State\nColumn 7: SignDate\nColumn 8: JobID\nColumn 9: JobCategoryID\n" +
+                        "Column 10: ProgramType\nColumn 11: CustomerName\nColumn 12: PrimaryTechnicianID\nColumn 13: DistrictName\n" +
+                        "Column 14: WOStatusID\n Column 15: Revenue\nColumn 16: Expense\n Column 17: GP");
+            } else {
+                System.out.println("Correct Format");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Unexpected error occurred while processing data: " + e);
+        }
+    } //processCSV
+
+
 } //Main
